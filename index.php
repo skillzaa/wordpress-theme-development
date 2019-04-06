ddcaf5f2a658d0a391f2b83ed540b3a67c670b47
skillzaa <49301266+skillzaa@users.noreply.github.com>
2019-04-06 14:27:35 +0500

    Create index.php
    First Index.php code

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>My Frist WordPress Theme</title>
+  <meta name="description" content="">
+  
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  <h1> Hello World</h1>
+</body>
+</html>
